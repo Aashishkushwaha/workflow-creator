2deda2716faa662489b7712d191961429f5e4415
Aashish Kushwaha <aashishk1997@gmail.com>
2020-06-05 01:03:11 +0530

    loader added to login

--- frontend/src/components/LoginPage/LoginPage.js
+++ frontend/src/components/LoginPage/LoginPage.js
@@ -1,15 +1,19 @@
 import React, { useContext } from "react";
 import Form from "../UI/Form/Form";
 import ModalContext from "../../context/ModalContext";
+import LoaderContext from "../../context/LoaderContext";
 import AuthContext from "../../context/AuthContext";
 import { BASE_URL } from "../../Api";
 
 const LoginPage = (props) => {
   let ModalContextValue = useContext(ModalContext);
   let AuthContextValue = useContext(AuthContext);
+  const { setShowLoader } = useContext(LoaderContext);
 
   const onSubmitHandler = async (formData) => {
     try {
+      setShowLoader(true);
+
       let response = await fetch(`${BASE_URL}/api/users/login`, {
         method: "POST",
         headers: {
@@ -19,12 +23,15 @@ const LoginPage = (props) => {
       });
 
       let res = await response.json();
-      
+
       if (res.message) {
         const data = { token: res.token, userId: res.userId };
         AuthContextValue.login(data);
+        setShowLoader(false);
         props.history.push("/workflow");
-        ModalContextValue.setModalContent("You have logged in successfully. 😊");
+        ModalContextValue.setModalContent(
+          "You have logged in successfully. 😊"
+        );
         ModalContextValue.setShowModal(true);
       } else {
         ModalContextValue.setModalContent(res.error);
